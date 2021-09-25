d4879750eaf92077a97083078198811c1dfd9284
rise-k <cjk254a@gmail.com>
2021-09-25 10:31:40 +0900

    first-commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class ="container">
+    <img src="../img/logo.png" alt="ロゴ">
+    <NuxtLink to ="/">新規登録</NuxtLink>
+    <NuxtLink to ="/login">ログイン</NuxtLink>
+    <div class ="card">
+      <p>新規登録</p>
+      <input type="text" placeholder="ユーザーネーム">
+      <input type="email" placeholder="メールアドレス">
+      <input type="password" placeholder="パスワード">
+      <button type="submit">新規登録</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container{
+  background: rgb(10, 10, 78);
+}
+</style>
